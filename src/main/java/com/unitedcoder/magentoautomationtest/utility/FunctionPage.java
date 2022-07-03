caaf43f7fc52b89a516248c9614f690e1b086dea
Umit Erkin <umut612n@gmail.com>
2022-07-03 18:37:29 +0200

    add new customer first time2

--- src/main/java/com/unitedcoder/magentoautomationtest/utility/FunctionPage.java
+++ src/main/java/com/unitedcoder/magentoautomationtest/utility/FunctionPage.java
@@ -1,5 +1,6 @@
 package com.unitedcoder.magentoautomationtest.utility;
 
+import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
