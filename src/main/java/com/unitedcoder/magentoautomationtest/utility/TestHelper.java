583e5819ae86e0b94e601d30b289d212c436e9a2
esmanurmemet <esmanur90@gmail.com>
2022-07-04 22:11:28 +0200

    CustomerGroup page -Esma-secound

---
+++ src/main/java/com/unitedcoder/magentoautomationtest/utility/TestHelper.java
@@ -0,0 +1,14 @@
+package com.unitedcoder.magentoautomationtest.utility;
+
+public class TestHelper {
+    private String customerGroupName;
+
+
+    public TestHelper(String customerGroupName) {
+        this.customerGroupName = customerGroupName;
+    }
+
+    public String getCustomerGroupName() {
+        return customerGroupName;
+    }
+}
