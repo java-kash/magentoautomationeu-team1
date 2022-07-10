974740798f9d2e0b15438ac0d352f9feb274dc91
Nijat <alper0818@gmail.com>
2022-07-11 01:19:52 +0200

    Store_Module_Edit

--- src/test/java/runner/cucumberframwork/Runner.java
+++ src/test/java/runner/cucumberframwork/Runner.java
@@ -1,4 +1,29 @@
 package runner.cucumberframwork;
 
-public class Runner {
+import com.unitedcoder.magentoautomationtest.utility.TestBase;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target//cucumber-output/cucumber-html-report.html",
+                "json:target/cucumber.json",
+                "junit:target/cucumber-results.xml"},
+        features = {"src/test/resources"},
+        tags=" @Login"
+)
+
+public class Runner extends TestBase {
+    @BeforeClass
+    public static void setup() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+    }
 }
