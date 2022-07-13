dde4937d4fb2dbbfd20e389e3634382480d03935
Umit Erkin <umut612n@gmail.com>
2022-07-13 20:12:13 +0200

    modify customer

--- src/main/java/com/unitedcoder/magentoautomationtest/backend/customersmodule/AssignCustomerToAGroup.java
+++ src/main/java/com/unitedcoder/magentoautomationtest/backend/customersmodule/AssignCustomerToAGroup.java
@@ -9,26 +9,22 @@ import org.openqa.selenium.support.PageFactory;
 public class AssignCustomerToAGroup {
     WebDriver driver;
     FunctionPage functionPage;
+    @FindBy(xpath="//*[@id=\"customerGrid_massaction-select\"]/option[5] ")
+    WebElement assignACustomerGroupOptions;
+    @FindBy(xpath="//*[@id=\"visibility\"]/option[3]")
+    WebElement groupListOptions;
+    @FindBy(xpath="//*[@id=\"customerGrid_table\"]/tbody/tr[1]/td[1]/input")
+    WebElement checkBoxButton;
+    @FindBy(xpath="//*[text()='Submit']")
+    WebElement submitButton;
+    @FindBy(xpath="//*[@id=\"messages\"]/ul/li")
+    WebElement successMessage;
 
     public AssignCustomerToAGroup(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver,this);
         functionPage=new FunctionPage(driver);
     }
-    @FindBy(xpath="//*[@id=\"customerGrid_massaction-select\"]/option[5] ")
-    WebElement assignACustomerGroupOptions;
-
-    @FindBy(xpath="//*[@id=\"visibility\"]/option[3]")
-    WebElement groupListOptions;
-
-    @FindBy(xpath="//*[@id=\"customerGrid_table\"]/tbody/tr[1]/td[1]/input")
-    WebElement checkBoxButton;
-
-    @FindBy(xpath="//*[text()='Submit']")
-    WebElement submitButton;
-
-    @FindBy(xpath="//*[@id=\"messages\"]/ul/li")
-    WebElement successMessage;
 
     public void assignGroup(){
         functionPage.waitForElement(assignACustomerGroupOptions);
--- src/main/java/com/unitedcoder/magentoautomationtest/backend/storemodule/ManageCurrencyRatesPage.java
+++ src/main/java/com/unitedcoder/magentoautomationtest/backend/storemodule/ManageCurrencyRatesPage.java
@@ -13,31 +13,24 @@ import org.openqa.selenium.support.PageFactory;
 public class ManageCurrencyRatesPage {
     WebDriver driver;
     FunctionPage functionPage;
+    @FindBy(linkText = "System")
+    WebElement systemTab;
+    @FindBy(linkText = "Manage Stores")
+    WebElement manageStoresOption;
+    @FindBy(linkText = "Catalog")
+    WebElement catalogTab;
+    @FindBy(linkText = "Manage Products")
+    WebElement manageProductsOption;
+    @FindBy(linkText = "Manage Categories")
+    WebElement manageCategoriesOption;
+    @FindBy(linkText = "Sales")
+    WebElement salesTab;
+    @FindBy(linkText = "Orders")
+    WebElement ordersOption;
 
     public ManageCurrencyRatesPage(WebDriver driver) {
         this.driver = driver;
         functionPage = new FunctionPage(driver);
         PageFactory.initElements(driver, this);
     }
-
-    @FindBy(linkText = "System")
-    WebElement systemTab;
-
-    @FindBy(linkText = "Manage Stores")
-    WebElement manageStoresOption;
-
-    @FindBy(linkText = "Catalog")
-    WebElement catalogTab;
-
-    @FindBy(linkText = "Manage Products")
-    WebElement manageProductsOption;
-
-    @FindBy(linkText = "Manage Categories")
-    WebElement manageCategoriesOption;
-
-    @FindBy(linkText = "Sales")
-    WebElement salesTab;
-
-    @FindBy(linkText = "Orders")
-    WebElement ordersOption;
 }
