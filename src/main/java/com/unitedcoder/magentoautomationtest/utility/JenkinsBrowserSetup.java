62d7f0d180d399701465c17778e6b8ef764f22a3
Umit Erkin <umut612n@gmail.com>
2022-07-16 15:05:34 +0200

    Jenkin_Setup, Modify Driver

--- src/main/java/com/unitedcoder/magentoautomationtest/utility/JenkinsBrowserSetup.java
+++ src/main/java/com/unitedcoder/magentoautomationtest/utility/JenkinsBrowserSetup.java
@@ -11,8 +11,9 @@ public class JenkinsBrowserSetup {
     public boolean serHeadlessModelIfLinux(ChromeOptions chromeOptions){
         System.out.println("User is Linux");
         String osname=System.getProperty("os.name");
+        WebDriverManager.chromedriver().setup();
         if(osname.toLowerCase().contains("linux")){
-            WebDriverManager.chromedriver().setup();
+
             System.out.println("Using headless browser mode");
             chromeOptions.addArguments(Arrays.asList("--headless","--disable-gpu"));
             chromeOptions.addArguments("window-size=1200,1100");
