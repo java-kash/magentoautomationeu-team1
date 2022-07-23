6d915c6d112de451d772f907d2273cac71b1cca5
esmanurmemet <esmanur90@gmail.com>
2022-07-23 23:58:38 +0200

    salesmodule -Esma

--- src/main/java/com/unitedcoder/magentoautomationtest/backend/salesmodule/SalesModuleInvoicesPage.java
+++ src/main/java/com/unitedcoder/magentoautomationtest/backend/salesmodule/SalesModuleInvoicesPage.java
@@ -3,17 +3,20 @@ package com.unitedcoder.magentoautomationtest.backend.salesmodule;
 import com.unitedcoder.magentoautomationtest.utility.FunctionPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SalesModuleInvoicesPage {
     WebDriver driver;
     FunctionPage functionPage;
+    Actions actions;
 
     public SalesModuleInvoicesPage(WebDriver driver) {
         this.driver = driver;
     PageFactory.initElements(driver,this);
         functionPage=new FunctionPage(driver);
+        actions=new Actions(driver);
     }
 
     @FindBy(xpath = "(//a[text()='View'])[1]")
@@ -22,6 +25,21 @@ public class SalesModuleInvoicesPage {
     WebElement salesTeb;
     @FindBy(xpath = "//*[text()='Invoices']")
     WebElement invoicesOption;
+    @FindBy(xpath = "(//*[@class='form-list'])[1]")
+    WebElement invoiceList;
+    @FindBy(id = "history_comment")
+    WebElement commentField;
+
+    public void salesManagerViewInvoices(){
+        functionPage.waitForElement(salesTeb);
+        actions.moveToElement(salesTeb).perform();
+        functionPage.waitForElement(invoicesOption);
+        invoicesOption.click();
+    }
+
+    public void invoiceAdComment(){
+
+    }
 
 
 }
