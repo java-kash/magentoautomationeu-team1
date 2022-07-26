ab4768b844cd2399fd6f148f1150d92182390f70
LTC <karsaylik@gmail.com>
2022-07-26 14:26:01 +0200

    In this branch I pushed these changes-ayimsa

---
+++ src/main/java/com/unitedcoder/magentoautomationtest/backend/reportingmodule/ReportingModuleDashBoard.java
@@ -0,0 +1,27 @@
+package com.unitedcoder.magentoautomationtest.backend.reportingmodule;
+
+import com.unitedcoder.magentoautomationtest.utility.FunctionPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ReportingModuleDashBoard {
+    WebDriver driver;
+    FunctionPage functionPage;
+
+    public ReportingModuleDashBoard(WebDriver driver) {
+        this.driver = driver;
+        functionPage=new FunctionPage(driver);
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//span[text()='Reports']")
+    WebElement reportsLink;
+    @FindBy(xpath = "//span[text()='Sales']")
+    WebElement salesLink;
+    @FindBy(xpath = "//span[text()='Orders']")
+    WebElement ordersLink;
+    @FindBy(xpath = "//span[text()='Refunds']")
+    WebElement refundsLink;
+
+}
---
+++ src/main/java/com/unitedcoder/magentoautomationtest/backend/reportingmodule/ReportsSalesRefundsPage.java
@@ -0,0 +1,4 @@
+package com.unitedcoder.magentoautomationtest.backend.reportingmodule;
+
+public class ReportsSalesRefundsPage {
+}
