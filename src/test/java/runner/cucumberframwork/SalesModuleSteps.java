ea2eb683e61bfa7e5e986677d21003b293b90bd6
sadikjan <palwan2010@live.nl>
2022-08-02 21:41:35 +0200

    MAGE2022-514-manager-should-be-able-to-update-scipments-Sadik

--- src/test/java/runner/cucumberframwork/SalesModuleSteps.java
+++ src/test/java/runner/cucumberframwork/SalesModuleSteps.java
@@ -16,6 +16,8 @@ import org.junit.Assert;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class SalesModuleSteps extends TestBase {
     SalesManagerLogin salesManagerLogin;
@@ -26,6 +28,10 @@ public class SalesModuleSteps extends TestBase {
     ScreenshotUtility screenshotUtility=new ScreenshotUtility();
     ManageOrdersPage manageOrdersPage;
     SalesModuleShipmentPage salesModuleShipmentPage;
+    EditOrdersWithInStorePickup editOrdersWithInStorePickup;
+    OrdersPage ordersPage;
+    CreditMemosPage creditMemosPage;
+    String title;
 
     @Before("@SalesModuleFeature")
     public void setUp() {
@@ -71,7 +77,9 @@ public class SalesModuleSteps extends TestBase {
     public void salesManagerIsOnTheSalesDashboardPage() {
         functionPage=new FunctionPage(driver);
         salesModuleInvoicesPage=new SalesModuleInvoicesPage(driver);
-
+        editOrdersWithInStorePickup=new EditOrdersWithInStorePickup(driver);
+        ordersPage = new OrdersPage(driver);
+        creditMemosPage = new CreditMemosPage(driver);
     }
 
     @When("sales manager can view invoices on the invoices page")
@@ -100,6 +108,8 @@ public class SalesModuleSteps extends TestBase {
         Assert.assertTrue(salesModuleInvoicesPage.verifyInvoiceComment());
     }
 
+
+    //Sales Manager should be able to create a new order-abdukerim
     @When("sales manager can create new order")
     public void salesManagerCanCreateNewOrder(){
         manageOrdersPage.createOrder();
@@ -110,6 +120,89 @@ public class SalesModuleSteps extends TestBase {
         Assert.assertTrue(manageOrdersPage.verifyCreateOrder());
     }
 
+    //Sales Manager should be able to cancel  order abdukerim
+    @When("sales manager can cancel order")
+    public void salesManagerCanCancelOrder() {
+        manageOrdersPage.deleteOrder();
+    }
+
+    @Then("sales manager should see cancel success massage")
+    public void salesManagerShouldSeeCancelSuccessMassage() {
+        Assert.assertTrue(manageOrdersPage.verifyCancelOrder());
+    }
+
+    @When("sales manager edit orders with in store pickup")
+    public void salesManagerEditOrdersWithInStorePickup() {
+        editOrdersWithInStorePickup.edit();
+    }
+
+    @Then("verify edit orders success")
+    public void verifyEditOrdersSuccess() {
+        editOrdersWithInStorePickup.editVerify();
+    }
+
+
+
+    @When("user selects Pending or Processing sections in Status drop down")
+    public void userSelectsPendingOrProcessingSectionsInStatusDropDown() {
+        ordersPage.selectPendingOrProcessingRandomly();
+    }
+
+    @And("Sales clicks on {string} button")
+    public void userClicksOnButton(String buttonName) {
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        functionPage.clickOnButton(buttonName);
+    }
+
+    @And("user clicks on any order in the list table")
+    public void userClicksOnAnyOrderInTheListTable() {
+        functionPage.sleep(2);
+        ordersPage.clickOnAnyOrder();
+    }
+
+    @Given("Sales navigates to {string} page")
+    public void userNavigatesToPage(String pageName) {
+        String name = functionPage.getPageNameH3(pageName);
+        System.out.println("Page name is: " + name);
+    }
+
+    @And("user gets the Credit Memos number on the table")
+    public void userGetsTheCreditMemosNumberInTable() {
+        creditMemosPage.getNumber();
+    }
+
+    @When("user enters order number in the Order field")
+    public void userFillsTheCreditMemosNumberInCreditMemosFieldColumn() {
+        functionPage.sleep(2);
+        creditMemosPage.fillNumberField();
+    }
+
+    @Then("user should be able to see new Credit Memos in the table list")
+    public void userShouldBeAbleToSeeNewCreditMemosInTheTableList() {
+        Assert.assertTrue(creditMemosPage.verifyCanSeeCreditMemos());
+    }
+
+    @When("Sales gets the title of the page")
+    public void user_gets_the_title_of_the_page() {
+        title = functionPage.getPageTitle();
+        System.out.println("Page title is: " + title);
+    }
+
+    @Then("Sales module page title should be {string}")
+    public void page_title_should_be(String expectedTitleName) {
+        Assert.assertTrue(title.contains(expectedTitleName));
+    }
+
+    @When("Sales hovers to click the {string}")
+    public void userHoversOverThe(String text) {
+        functionPage.hoverToClick(text);
+    }
+
+
+    @Then("Sales should be able to see success message {string}")
+    public void salesShouldBeAbleToSeeSuccessMessage(String successMessage) {
+        Assert.assertTrue(functionPage.getSuccessMessage(successMessage));
+    }
 
     @When("sales manager can update tracking and history information shipments{string}")
     public void salesManagerCanUpdateTrackingAndHistoryInformationShipmentsAnd(String arg0) {
@@ -120,4 +213,5 @@ public class SalesModuleSteps extends TestBase {
     public void salesManagerShouldBeAbleToCommentsToShipments() {
         salesModuleShipmentPage.verifyShipmentPage();
     }
+
 }
